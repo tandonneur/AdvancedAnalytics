49be1f07aa20429f19a4d1b4ed95281f179fc53e
Edward R Jones <tandonneur@gmail.com>
2019-06-02 19:01:11 -0500

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as f:
     long_description = f.read()
 
-setuptools.setup(
+setup(
         name="AdvancedAnalytics", 
-        version="0.1.4", 
+        version="1.7", 
         author="Edward R Jones", 
         author_email="ejones@tamu.edu", 
-        url="http://github.com/tandonneur/AdvancedAnalytics", 
+        url="https://github.com/tandonneur/AdvancedAnalytics", 
         description="Python support for 'The Art and Science of Data Analytics'",
+	keywords="Statistics Art Science Data Analytics",
         long_description=long_description,
         long_description_content_type="text/markdown",
-        packages=setuptools.find_packages(),
+	py_modules=["AdvancedAnalytics" ],
+        packages=find_packages(),
 	python_requires=">=3.5",
-	install_requires=["numpy>=1.15"], 
         classifiers=[
                 "Programming Language :: Python :: 3",
                 "License :: OSI Approved :: MIT License",
