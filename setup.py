df24672ba29125175d8312044f6170f329b8d634
Edward R Jones <tandonneur@gmail.com>
2020-05-24 05:41:21 -0500

    Add files via upload
    Changed version from 1.4 to 1.9

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.rst", "r") as f:
 
 setuptools.setup(
     name="AdvancedAnalytics", 
-    version="1.4", 
+    version="1.9", 
     author="Edward R Jones", 
     author_email="ejones@tamu.edu", 
     url="https://github.com/tandonneur/AdvancedAnalytics", 
